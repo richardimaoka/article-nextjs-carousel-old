243a08ca8edd5559da2e1e2ad9deb0439a1bbfd2
Richard Imaoka <richard.e.imaoka@gmail.com>
2024-01-21 13:29:35 +0900

    Carousel.module.css

--- next/app/components/Carousel.tsx
+++ next/app/components/Carousel.tsx
@@ -1,8 +1,9 @@
+import styles from "./Carousel.module.css";
 import Image from "next/image";
 
 export function Carousel() {
   return (
-    <div>
+    <div className={styles.component}>
       <Image width={640} height={360} src="/images/1.png" alt="1.png" />
       <Image width={640} height={360} src="/images/2.png" alt="2.png" />
       <Image width={640} height={360} src="/images/3.png" alt="3.png" />
--- next/app/page.tsx
+++ next/app/page.tsx
@@ -2,7 +2,7 @@ import { Carousel } from "./components/Carousel";
 
 export default function Page() {
   return (
-    <main style={{ width: "640px", margin: "auto" }}>
+    <main>
       <Carousel />
     </main>
   );
