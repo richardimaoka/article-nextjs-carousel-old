6b40da952b8a1b937d88aae8ba119eda7ed65b3a
Richard Imaoka <richard.e.imaoka@gmail.com>
2024-01-21 13:25:37 +0900

    images 1 2 3

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -4,6 +4,8 @@ export default function Page() {
   return (
     <main style={{ width: "640px", margin: "auto" }}>
       <Image width={640} height={360} src="/images/1.png" alt="1.png" />
+      <Image width={640} height={360} src="/images/2.png" alt="2.png" />
+      <Image width={640} height={360} src="/images/3.png" alt="3.png" />
     </main>
   );
 }
