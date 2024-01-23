5d7950e264da43629f0bd9599027700fa1f45a45
Richard Imaoka <richard.e.imaoka@gmail.com>
2024-01-23 21:04:20 +0900

    add buttons

--- next/app/components/Carousel.tsx
+++ next/app/components/Carousel.tsx
@@ -18,6 +18,11 @@ export function Carousel(props: Props) {
           />
         ))}
       </div>
+      <div>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+      </div>
     </div>
   );
 }
